6bba2710802a06019547fb96a2cd78901dc4c78d
yani-again <yanislavyankov23@gmail.com>
2026-08-22 23:01:42 +0100

    outlined basic API calls and typedefs

---
+++ src/y_lpuart.c
@@ -0,0 +1,22 @@
+#include "y_lpuart.h"
+
+YLPUART_returnStatus ylpuart_setConfig(YLPUART_typeDef* handler)
+{
+    /* TODO: implement */
+
+    return YLPUART_error;
+}
+
+YLPUART_status ylpuart_transmit(YLPUART_typeDef* handler, uint8_t* pData, uint16_t dataSize)
+{
+    /* TODO: implement */
+
+    return YLPUART_off;
+}
+
+YLPUART_status ylpuart_receive(YLPUART_typeDef* handler, uint8_t* pData, uint16_t dataSize)
+{
+    /* TODO: implement */
+
+    return YLPUART_off;
+}
